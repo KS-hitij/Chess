3b6c9a117377066ce78b666d7a9f2c8781c3854a
KS-hitij <149416585+KS-hitij@users.noreply.github.com>
2025-07-28 15:22:52 +0530

    added Article type

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,24 @@ import { getServerSession } from "next-auth";
 import SignInBtn from "./components/SignInBtn";
 import SignOutBtn from "./components/SignOutBtn";
 import { authOptions } from "./api/auth/[...nextauth]/authOptions";
+
+type Article = {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string; 
+  content: string;
+};
+
 export default async function Home() {
   const result = await axios.get(`https://newsapi.org/v2/everything?q="Chess Tournament"&sortBy=popularity&pageSize=4&apiKey=${process.env.NEWS_API_KEY}`);
-  const articles = result.data.articles;
+  const articles:Article[] = result.data.articles;
   const session = await getServerSession(authOptions);
   return (
     <div className="min-h-screen max-w-screen bg-base-300 overflow-x-hidden">
