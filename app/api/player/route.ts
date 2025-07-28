29ca8bab1fdc01f64c35d9f0d945d5722d6311ae
KS-hitij <149416585+KS-hitij@users.noreply.github.com>
2025-07-28 15:32:55 +0530

    fixed folder structure

--- app/api/player/route.ts
+++ app/api/player/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/database/db";
 import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/authOptions";
 
 export async function GET(req:Request){
     try{
-        const session = await getServerSession();
+        const session = await getServerSession(authOptions);
     if(!session){
         return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
