d609397b850e1c9e660746eb74a26598815e12a9
KS-hitij <149416585+KS-hitij@users.noreply.github.com>
2025-07-28 15:14:51 +0530

    fixed

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,59 @@
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/app/lib/database/db";
+import { AuthOptions } from "next-auth";
+const authOptions:AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        })
+    ],
+    secret:process.env.NEXT_AUTH_SECRET || "",
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account?.provider === "google") {
+                const email = profile?.email;
+                const name = profile?.name;
+                const username = `${name}${crypto.randomUUID().split('-')[0]}`;
+                try {
+                    const existingUser = await prisma.user.findFirst({
+                        where: { email }
+                    });
+                    if (existingUser) {
+                        return true;
+                    }
+                    await prisma.user.create({
+                        data: {
+                            email: email || "",
+                            username,
+                            name: name || "",
+                        }
+                    })
+                    return true;
+                }catch(err){
+                    console.error(err);
+                    return false;
+                }
+            }
+            return false;
+        },
+        async jwt({token,profile}){
+            if(profile){
+                token.email = profile.email;
+            }
+            return token;
+        },
+        async session({session,token}){
+            if(token){
+                if(session.user)
+                    session.user.email = token.email;
+            }
+            return session;
+        }
+    },
+    
+    session:{
+        strategy:"jwt"
+    }
+}
+export {authOptions};
